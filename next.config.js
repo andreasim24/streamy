27ece7886f6c45585b1af0ac2855fad4b6c7ac4a
andreasim24 <andreasim2426@gmail.com>
2022-09-13 15:24:42 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn.pixabay.com",
+      "p16-amd-va.tiktokcdn.com",
+      "image.shutterstock.com",
+      "img.freepik.com"
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
